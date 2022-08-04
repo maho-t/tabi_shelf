709039b052f399180aa9410defc5659582b17e3a
pipimaho <mtateno0@gmail.com>
2022-08-04 17:58:01 +0900

    モデルの定義

--- shelves/models.py
+++ shelves/models.py
@@ -1,3 +1,17 @@
+from tabnanny import verbose
+from turtle import mode
 from django.db import models
 
-# Create your models here.
+class Tabi(models.Model):
+  title = models.CharField(max_length=30)
+  date = models.DateField
+  place = models.CharField(verbose_name='PLACE', max_length=20)
+  genre = models.CharField(verbose_name='GENRE', max_length=40)
+  accompany = models.CharField(verbose_name='PEOPLE', max_length=10)
+  text = models.TextField
+
+  class Meta:
+    verbose_name_plural = 'Tabi_data'
+
+  def __str__(self):
+    return self.title
