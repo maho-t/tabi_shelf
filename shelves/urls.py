47984b68cc7e8334d3dd041dafa0158c68df7d2d
pipimaho <mtateno0@gmail.com>
2022-08-07 20:46:19 +0900

    urlsの修正

--- shelves/urls.py
+++ shelves/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import TabiList
 
 urlpatterns = [
-  path('', TabiList.as_views(), name='list'),
+  path('', TabiList.as_view(), name='list'),
 ]
