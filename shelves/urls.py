7c5c882f990404a7a50778bba23542a3b40014bb
pipimaho <mtateno0@gmail.com>
2022-08-12 15:57:22 +0900

    詳細ページの作成

--- shelves/urls.py
+++ shelves/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TabiList
+from .views import TabiDetail, TabiList
 
 urlpatterns = [
   path('', TabiList.as_view(), name='list'),
+  path('detail/<int:pk>', TabiDetail.as_view(), name='detail'),
 ]
--- shelves/views.py
+++ shelves/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Tabi
 
 class TabiList(ListView):
   model = Tabi
   context_object_name = "journeys"
+
+class TabiDetail(DetailView):
+  model = Tabi
+  context_object_name = "journey"
