c8a4551ae0f1a79fe9efd8d8ea6374b32d5193f9
pipimaho <mtateno0@gmail.com>
2022-08-13 11:15:03 +0900

    新規作成機能の実装

--- shelves/urls.py
+++ shelves/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TabiDetail, TabiList
+from .views import TabiDetail, TabiList, TabiCreate
 
 urlpatterns = [
   path('', TabiList.as_view(), name='list'),
   path('detail/<int:pk>', TabiDetail.as_view(), name='detail'),
+  path('create/', TabiCreate.as_view(), name='create'),
 ]
--- shelves/views.py
+++ shelves/views.py
@@ -1,5 +1,8 @@
+import imp
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+
+from django.urls import reverse_lazy
 
 from .models import Tabi
 
@@ -10,3 +13,8 @@ class TabiList(ListView):
 class TabiDetail(DetailView):
   model = Tabi
   context_object_name = "journey"
+
+class TabiCreate(CreateView):
+  model = Tabi
+  fields = "__all__"
+  success_url = reverse_lazy('list')
