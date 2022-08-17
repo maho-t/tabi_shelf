553dc5af6dab0b0f9f7727a0901a153856c89bed
pipimaho <mtateno0@gmail.com>
2022-08-18 08:09:08 +0900

    選択肢の記述

--- shelves/forms.py
+++ shelves/forms.py
@@ -10,3 +10,26 @@ class TabiModelForm(forms.ModelForm):
     label = '日付',
     widget=DatePickerInput(format='%Y-%m-%d')
   )
+
+class TabiModelForm(forms.ModelForm):
+  genre = forms.fields.ChoiceField(
+    choice_genre = (
+      ('1', '体験／教室'),
+      ('2', 'スポーツ／観戦'),
+      ('3', '自然／景色／花'),
+      ('4', '動物'),
+      ('5', '宿泊'),
+      ('6', 'グルメ'),
+      ('7', '文化／芸術'),
+      ('8', '祭り'),
+      ('9', 'テーマパーク'),
+      ('10', 'ショッピング'),
+      ('11', '世界遺産／遺産'),
+      ('12', '乗り物'),
+      ('13', 'スパ／エステ／温泉'),
+      ('14', '学習／研修'),
+      ('15', 'その他')
+    ),
+    required=True,
+    widget=forms.widgets.Select
+  )
