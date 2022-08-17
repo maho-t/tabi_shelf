32b7fbfce330ab9bc2fe3f71d3c82fef7b40732a
pipimaho <mtateno0@gmail.com>
2022-08-18 08:31:56 +0900

    選択肢の作成

--- shelves/forms.py
+++ shelves/forms.py
@@ -2,34 +2,11 @@ from dataclasses import field
 from pyexpat import model
 from tkinter import Widget
 from django import forms
-from bootstrap_datepicker_plus import DatePickerInput
+from bootstrap_datepicker_plus.widgets import DatePickerInput
 from .models import Tabi
 
 class TabiModelForm(forms.ModelForm):
   date = forms.DateField(
     label = '日付',
     widget=DatePickerInput(format='%Y-%m-%d')
-  )
-
-class TabiModelForm(forms.ModelForm):
-  genre = forms.fields.ChoiceField(
-    choice_genre = (
-      ('1', '体験／教室'),
-      ('2', 'スポーツ／観戦'),
-      ('3', '自然／景色／花'),
-      ('4', '動物'),
-      ('5', '宿泊'),
-      ('6', 'グルメ'),
-      ('7', '文化／芸術'),
-      ('8', '祭り'),
-      ('9', 'テーマパーク'),
-      ('10', 'ショッピング'),
-      ('11', '世界遺産／遺産'),
-      ('12', '乗り物'),
-      ('13', 'スパ／エステ／温泉'),
-      ('14', '学習／研修'),
-      ('15', 'その他')
-    ),
-    required=True,
-    widget=forms.widgets.Select
   )
--- shelves/models.py
+++ shelves/models.py
@@ -1,10 +1,28 @@
 from django.db import models
 
 class Tabi(models.Model):
+  GENRE = (
+      ('1', '体験／教室'),
+      ('2', 'スポーツ／観戦'),
+      ('3', '自然／景色／花'),
+      ('4', '動物'),
+      ('5', '宿泊'),
+      ('6', 'グルメ'),
+      ('7', '文化／芸術'),
+      ('8', '祭り'),
+      ('9', 'テーマパーク'),
+      ('10', 'ショッピング'),
+      ('11', '世界遺産／遺産'),
+      ('12', '乗り物'),
+      ('13', 'スパ／エステ／温泉'),
+      ('14', '学習／研修'),
+      ('15', 'その他'),
+  )
+
   title = models.CharField(verbose_name='旅タイトル', max_length=30)
   date = models.DateField(verbose_name='日付', auto_now=False)
   place = models.CharField(verbose_name='場所', max_length=20)
-  genre = models.CharField(verbose_name='ジャンル', max_length=40)
+  genre = models.CharField(verbose_name='ジャンル', choices=GENRE, max_length=40)
   accompany = models.CharField(verbose_name='同行者', max_length=10)
   text = models.TextField(verbose_name='メモ')
 
