0d681336dc6a80def7c5542682de0a7c691a77e5
pipimaho <mtateno0@gmail.com>
2022-09-01 06:01:38 +0900

    ビューの実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import imp
+from multiprocessing import context
+from urllib import request
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.contrib.auth.forms import AccountsCreationForm
+from django.urls import is_valid_path
 
-# Create your views here.
+def register(request):
+  if request.method != 'POST':
+    form = AccountsCreationForm()
+  else:
+    form = AccountsCreationForm(data=request.POST)
+    if form.is_valid():
+      new_user = form.save()
+      login(request, new_user)
+      return redirect('shelves:index')
+
+  context = {'form': form}
+  return render(request, 'registration/register.html', context)
