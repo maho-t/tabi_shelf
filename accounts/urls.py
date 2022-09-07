5cb3f7254ebf43cc8b1c7c93813751c9b3bc8f56
pipimaho <mtateno0@gmail.com>
2022-09-08 08:54:59 +0900

    登録画面の実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import URLPattern, path, include
 from . import views
 
 app_name = 'accounts'
-ulrpatterns = [
+urlpatterns = [
   path('', include('django.contrib.auth.urls')),
   path('register/', views.register, name='register'),
 ]
